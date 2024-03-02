a80df296187934517642bf72d9fa3906790ebc93
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-03-02 20:47:46 +0100

    Planet class created and lesson1 fix

--- src/enumPractice/Lesson1.java
+++ src/enumPractice/Lesson1.java
@@ -11,15 +11,40 @@ public class Lesson1 {
         myPlanet = Planet.valueOf(scanner.nextLine().toUpperCase());
 
         switch (myPlanet){
-            case MARS -> System.out.println("Human cannot survive there");
-            case EARTH -> System.out.println("Human can survive and live there");
-            case PLUTO -> System.out.println("Human cannot live there yet");
-            case MERCURY -> System.out.println("Human cannot live there yet");
-            case SATURN -> System.out.println("Human cannot live there yet");
-            case NEPTUNE -> System.out.println("Human cannot live there yet");
-            case VENUE -> System.out.println("Human cannot live there yet");
-            case URANUS -> System.out.println("Human cannot live there yet");
-            case JUPITER -> System.out.println("Human cannot live there yet");
+            case MARS -> {
+                System.out.println("Human cannot survive there");
+                System.out.println("Number # " + myPlanet.getNumber());
+            }
+            case EARTH -> {
+                System.out.println("Human can survive and live there");
+                System.out.println("Number # " + myPlanet.getNumber());
+            }
+            case PLUTO -> {
+                System.out.println("Human cannot live there yet");
+                System.out.println("Number # " + myPlanet.getNumber());
+            }
+            case MERCURY -> {
+                System.out.println("Human cannot live there yet");
+                System.out.println("Number # " + myPlanet.getNumber());
+            }
+            case SATURN -> {
+                System.out.println("Human cannot live there yet");
+                System.out.println("Number # " + myPlanet.getNumber());
+            }
+            case NEPTUNE -> {
+                System.out.println("Human cannot live there yet");
+                System.out.println("Number # " + myPlanet.getNumber());
+            }
+            case VENUE -> {
+                System.out.println("Human cannot live there yet");
+                System.out.println("Number # " + myPlanet.getNumber());
+            }
+            case URANUS, JUPITER -> {
+                System.out.println("Human cannot live there yet");
+                System.out.println("Number # " + myPlanet.getNumber());
+            }
+            default -> System.out.println("Not a valid planet");
+
         }
 
         System.out.println(myPlanet);
--- src/enumPractice/Planet.java
+++ src/enumPractice/Planet.java
@@ -1,13 +1,20 @@
 package enumPractice;
 
 public enum Planet {
-    EARTH,
-    JUPITER,
-    MARS,
-    MERCURY,
-    SATURN,
-    NEPTUNE,
-    VENUE,
-    URANUS,
-    PLUTO
+    EARTH(1),
+    JUPITER(2),
+    MARS(3),
+    MERCURY(4),
+    SATURN(5),
+    NEPTUNE(6),
+    VENUE(7),
+    URANUS(8),
+    PLUTO(9);
+
+    private int number;
+
+    Planet(int number) {
+        this.number = number;
+    }
+    public int getNumber(){ return number; }
 };
